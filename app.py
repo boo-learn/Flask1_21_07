936425cf081ad56348007491f1fe73a85b489cc1
boo <booblegum42@gmail.com>
2023-07-24 23:51:25 +0700

    module-1 tasks

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-from flask import Flask
+from flask import Flask, json
 
 app = Flask(__name__)
+json.provider.DefaultJSONProvider.ensure_ascii = False
+
+about_me = {
+    "name": "Евгений",
+    "surname": "Юрченко",
+    "email": "eyurchenko@specialist.ru"
+}
+
+quotes = [
+    {
+        "id": 3,
+        "author": "Rick Cook",
+        "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+    },
+    {
+        "id": 5,
+        "author": "Waldi Ravens",
+        "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+    },
+    {
+        "id": 6,
+        "author": "Mosher’s Law of Software Engineering",
+        "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+    },
+    {
+        "id": 8,
+        "author": "Yoggi Berra",
+        "text": "В теории, теория и практика неразделимы. На практике это не так."
+    },
+
+]
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!!!</p>"
+
+
+# http://127.0.0.1:5000/about
+@app.route("/about")
+def about_author():
+    return about_me
